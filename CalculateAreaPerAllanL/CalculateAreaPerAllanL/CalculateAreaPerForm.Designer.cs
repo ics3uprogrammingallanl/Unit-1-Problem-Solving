c4ee524125a17035237fc8ad89969f7fb216d0c3
AL10120 <allan.leveille@ocsbstudent.ca>
2019-02-22 12:00:38 -0500

    Did stuff that is insignificant

---
+++ CalculateAreaPerAllanL/CalculateAreaPerAllanL/CalculateAreaPerForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CalculateAreaPerAllanL
+{
+    partial class CalculateAreaPerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLength = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblAreaAnswer = new System.Windows.Forms.Label();
+            this.lblPerimeterAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(154, 163);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(40, 13);
+            this.lblLength.TabIndex = 0;
+            this.lblLength.Text = "Length";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(255, 160);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(467, 160);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(209, 92);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(255, 232);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 3;
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(154, 235);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(35, 13);
+            this.lblWidth.TabIndex = 4;
+            this.lblWidth.Text = "Width";
+            // 
+            // lblAreaAnswer
+            // 
+            this.lblAreaAnswer.AutoSize = true;
+            this.lblAreaAnswer.Location = new System.Drawing.Point(154, 368);
+            this.lblAreaAnswer.Name = "lblAreaAnswer";
+            this.lblAreaAnswer.Size = new System.Drawing.Size(64, 13);
+            this.lblAreaAnswer.TabIndex = 6;
+            this.lblAreaAnswer.Text = "The Area is:";
+            // 
+            // lblPerimeterAnswer
+            // 
+            this.lblPerimeterAnswer.AutoSize = true;
+            this.lblPerimeterAnswer.Location = new System.Drawing.Point(154, 313);
+            this.lblPerimeterAnswer.Name = "lblPerimeterAnswer";
+            this.lblPerimeterAnswer.Size = new System.Drawing.Size(86, 13);
+            this.lblPerimeterAnswer.TabIndex = 7;
+            this.lblPerimeterAnswer.Text = "The Perimeter is:";
+            this.lblPerimeterAnswer.Click += new System.EventHandler(this.lblPerimeterAnswer_Click);
+            // 
+            // CalculateAreaPerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPerimeterAnswer);
+            this.Controls.Add(this.lblAreaAnswer);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lblLength);
+            this.Name = "CalculateAreaPerForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblAreaAnswer;
+        private System.Windows.Forms.Label lblPerimeterAnswer;
+    }
+}
+
---
+++ CalculateAreaPerAllanL/CalculateAreaPerAllanL/CalculateAreaPerForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateAreaPerAllanL
+{
+    public partial class CalculateAreaPerForm : Form
+    {
+        public CalculateAreaPerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPerimeterAnswer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            double length, width;
+
+            length = double.Parse(txtLength.Text);
+
+
+
+            txtLength.Select();
+
+            txtWidth.Select();
+
+            this.lblPerimeterAnswer.Show();
+        }
+    }
+}
